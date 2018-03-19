66c514ddaaef6ed6d23e87aa978597ec11f0064d
Garret Richardson <garret.richardson.481@my.csun.edu>
2018-03-19 15:42:44 -0700

    button state as a struct

--- button_test/button_test.ino
+++ button_test/button_test.ino
@@ -1,60 +1,56 @@
 /*
-  Button
-
- Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
+  Sends a keyboard message per button press to the Android device.
 
 
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
+  The circuit:
+   pushbutton attached to pin 2-11 from +5V
+   10K resistor attached to pin 2-11 from ground
 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
+  created 2018
+  by grichardson108 <http://www.github.com/gRichardson108>
 
-
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Button
- */
+*/
 #include <stdio.h>
 #include <string.h>
 
-// constants won't change. They're used here to
-// set pin numbers:
-const int buttonPin[] = {2,3,4,5,6,7,8,9,10,11};     // the number of the pushbutton pin
-const int BUTTON_COUNT = 10;
-const int BUFFER_SIZE = 20;
-char buffer[BUFFER_SIZE] = "\0";
-//const int ledPin =  13;      // the number of the LED pin
+typedef struct ButtonState {
+  int pin_number;
+  int previous_state;
+  int current_state;
+} ButtonState;
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+#define BUTTON_COUNT 10
+#define BUFFER_SIZE 20
+ButtonState buttons[BUTTON_COUNT];
+const int BUTTON_PINS[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // the number of the pushbutton pin
+char buffer[BUFFER_SIZE] = "\0";
 
 void setup() {
-  Serial.begin(9600);
-  
-  // initialize the pushbutton pin as an input:
-  for (int i = 0; i < BUTTON_COUNT; i++){
-    pinMode(buttonPin[i], INPUT);
+  // initialize the pushbuttons as an input:
+  for (int i = 0; i < BUTTON_COUNT; i++) {
+    buttons[i].pin_number = BUTTON_PINS[i];
+    buttons[i].previous_state = LOW;
+    buttons[i].current_state = LOW;
+    pinMode(buttons[i].pin_number, INPUT);
   }
+  Serial.begin(9600);
 }
 
 void loop() {
-  memset(buffer, '\0', BUFFER_SIZE*sizeof(char));
+  //clear the current state of the buffer before reading/writing
+  memset(buffer, '\0', BUFFER_SIZE * sizeof(char));
   // read the state of the pushbutton value:
-  for (int i = 0; i < BUTTON_COUNT; i++){
-    buttonState = digitalRead(buttonPin[i]);
-    if (buttonState == HIGH) {
-      // turn LED on:
-      itoa(buttonPin[i], buffer, 10);
+  for (int i = 0; i < BUTTON_COUNT; i++) {
+    buttons[i].current_state = digitalRead(buttons[i].pin_number);
+    if (buttons[i].current_state == HIGH && buttons[i].previous_state == LOW) {
+      itoa(buttons[i].pin_number, buffer, 10);
       Serial.write(buffer);
     }
+    if (buttons[i].current_state == LOW && buttons[i].previous_state == HIGH) {
+      itoa(buttons[i].pin_number, buffer, 10);
+      Serial.write(buffer);
+    }
+    
+    buttons[i].previous_state = buttons[i].current_state;
   }
 }
